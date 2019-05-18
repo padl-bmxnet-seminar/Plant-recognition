431aa352f92855a4e6c5a9e8b713e7b9a07e88f4
Cornelius Hagmeister <cornelius.hagmeister@googlemail.com>
2019-05-18 15:02:57 +0200

    Add resnet train setup

---
+++ run.py
@@ -0,0 +1,94 @@
+import mxnet as mx
+import resnet
+import fit
+import argparse
+
+
+def gpu_device(gpu_number=0):
+    try:
+        _ = mx.nd.array([1, 2, 3], ctx=mx.gpu(gpu_number))
+    except mx.MXNetError:
+        return None
+    return mx.gpu(gpu_number)
+
+
+def get_iterators(train_batch_size, val_batch_size, height, width, train_path, val_path):
+    train_iter = mx.image.ImageIter(train_batch_size, (3, height, width),
+                                    path_root=".",
+                                    path_imglist=train_path,
+                                    aug_list=[
+                                        mx.image.SequentialAug([
+                                            mx.image.CastAug(typ='float32'),
+                                            # mx.image.ResizeAug(100),
+                                            mx.image.RandomCropAug([height, width])
+                                        ])
+                                    ]
+                                    )
+
+    val_iter = mx.image.ImageIter(val_batch_size, (3, height, width),
+                                  path_root=".",
+                                  path_imglist=val_path,
+                                  aug_list=[
+                                      mx.image.SequentialAug([
+                                          mx.image.CastAug(typ='float32'),
+                                          # mx.image.ResizeAug(100),
+                                          mx.image.RandomCropAug([height, width])
+                                      ])
+                                  ]
+                                  )
+    return train_iter, val_iter
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="resnet on clef dataset",
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--height', type=int, default=290,
+                        help='height of the training data')
+
+    parser.add_argument('--width', type=int, default=290,
+                        help='width of the training data')
+
+    parser.add_argument('--train_batch_size', type=int, default=5,
+                        help='batch size of train iterator')
+
+    parser.add_argument('--val_batch_size', type=int, default=5,
+                        help='batch size of val iterator')
+
+    parser.add_argument('--num_epochs', type=int, default=5,
+                        help='num of epochs')
+
+    parser.add_argument('--train_path', default="train_data_list.lst",
+                        help='relative path of train.lst')
+
+    parser.add_argument('--val_path', default="val_data_list.lst",
+                        help='relative path of val.lst')
+
+    # parser.add_argument('--gpus', default='',
+    #                     help='Gpus that are used e.g. 0,1')
+
+    fit.add_fit_args(parser)
+
+    parser.set_defaults(
+        # network
+        network='resnet',
+        num_layers=18,
+        # data
+        num_classes=250,
+        num_examples=16636,
+        min_random_scale=1,
+        lr=0.00001,
+        disp_batches=5,
+        monitor=1,
+        # train
+        lr_step_epochs='30,60',
+        dtype='float32',
+    )
+
+    args = parser.parse_args()
+    args.batch_size = args.train_batch_size
+    args.image_shape = '3,{},{}'.format(args.height, args.width)
+    train_iter, val_iter = get_iterators(args.train_batch_size, args.val_batch_size, args.height, args.width,
+                                         args.train_path, args.val_path)
+    sym = resnet.get_symbol(**vars(args))
+
+    fit.fit(args, sym, train_iter, val_iter)
